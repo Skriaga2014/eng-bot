e5a63c02f5abf5896ae878afdaf69130a0471dc1
skriaga <ilia.salomatin.1987@gmail.com>
2023-01-19 22:13:34 -0500

    plus sample

--- bot.py
+++ bot.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
+#'5891767200:AAGSRE4qcouHrjJiwXjV4-nW2Ro_endLw0M'
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.utils.markdown import hide_link
+from aiogram import Bot, Dispatcher, executor, types
+import const
+from questions import q
+import random
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+bot = Bot(token=const.token)
+dp = Dispatcher(bot)
+
+#global var
+var = 0
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer_photo(open(f"images/intro.jpg", 'rb'))
+    await message.answer("Hi, I'm a PDD_Bot! \nFor beginning send any message:")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# urlkb = InlineKeyboardMarkup(row_width=1, width='100%')
+# urlButton = InlineKeyboardButton(text='Перейти в блог Skillbox', url='https://skillbox.ru/media/code/')
+# urlButton2 = InlineKeyboardButton(text='12345678901234567890123456789012345678901234', url='https://skillbox.ru/code/')
+# urlkb.add(urlButton, urlButton2)
 
 
-# Press the green button in the gutter to run the script.
+@dp.message_handler(commands='task')
+async def url_command(message: types.Message):
+    await message.answer_photo(open(f"images/intro.jpg", 'rb'))
+    await message.answer('Полезные ссылки:', reply_markup=taskkb)
+
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    global var
+    if var != 0:
+        if message.text == q[var][0]:
+            await message.answer("It's right!")
+        else:
+            await message.answer(f'its a wrong answer (right {q[var][0]})')
+
+    #for i in range(10):
+    var = random.choice(list(q.keys()))
+    #await message.answer(f"{var}/{list(q.keys())}")
+    vars = q[var][1]
+    random.shuffle(vars)
+
+    kb = [
+        [types.KeyboardButton(text=vars[0])],
+        [types.KeyboardButton(text=vars[1])],
+        [types.KeyboardButton(text=vars[2])],
+        [types.KeyboardButton(text=vars[3])]
+        ]
+    keyboard = types.ReplyKeyboardMarkup(keyboard=kb, row_width=1)
+    #await bot.send_photo(photo='images/1000.jpg')
+    await message.answer_photo(open(f"images/{var}.jpg", 'rb'))
+    await message.answer(f'Question {var} must be here', reply_markup=keyboard)
+
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    executor.start_polling(dp, skip_updates=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
+
+
+
---
+++ eng.py
@@ -0,0 +1,141 @@
+"""
+Программа изучения слов иностранного языка.
+Выводит случайное слово из числа (VARS_NUM) слов, которые были верно переведены минимальное число раз.
+Направление перевода (на русский или с русского) выбирается случайным образом.
+"""
+
+import pandas as pd
+import datetime as dt
+import os
+import random as rnd
+
+
+# load dictionary
+def get_base():
+    # проверка, есть ли nan в shows и/или result
+    try:
+        base = pd.read_csv(BASE_FILE, dtype={'shows': int, 'result': int})
+    except ValueError:  # если nan найдено
+        base = pd.read_csv(BASE_FILE).fillna(0)
+        base = base.astype({'shows': 'int32', 'result': int})
+    except KeyError:    # если проблема с базой в результате сбоя предыдущей выгрузки
+        base = pd.read_csv(BASE_FILE_RESERVE).fillna(0)
+        base = base.astype({'shows': 'int32', 'result': int})
+
+    if len(base) < VARS_NUM:    # если количество слов в базе меньше
+        vars_num = len(base)
+    else:
+        vars_num = VARS_NUM
+
+    base = base.sort_values('result')
+    base_new = base.head(vars_num).sample(frac=1)
+    base_add = base.tail(len(base) - vars_num)
+
+    return base_new, base_add
+
+
+# send to log
+def to_log(log):    # date,variants, type,id,task,target,answer,result
+    line = f'\n{dt.datetime.now()},{SHOW_VARIANTS},'
+    log_items = ['lang', 'idx', 'task', 'right_answer', 'answer', 'check']
+    line += ','.join([str(log[i]) for i in log_items])
+
+    with open(LOG_FILE, 'a') as f:
+        f.write(line)
+
+
+# update my dict
+def base_update(base, base_add, log):
+    base.loc[base['id'] == log['idx'], ['shows']] += 1
+    if log['check']:
+        base.loc[base['id'] == log['idx'], ['result']] += 1
+    base_final = pd.concat([base, base_add])
+
+    base_final.to_csv(BASE_FILE, index=0)
+    base_final.to_csv(BASE_FILE_RESERVE, index=0)
+
+
+# get word for translate
+def get_task(base, lang):
+    sample = base.sample()
+    if lang == 'en':
+        task = sample['translate'].iloc[0]
+        smpl = sample['sample'].iloc[0]
+        #task = f'{task}\n{smpl}'
+        right_answer = sample['word'].iloc[0]
+    else:
+        task = sample['word'].iloc[0]
+        right_answer = sample['translate'].iloc[0]
+    if '/' in task:
+        task = task.split('/')
+    idx = sample['id'].iloc[0]
+    return idx, task, right_answer, smpl
+
+
+# check answer right or wrong
+def get_check(answer, right_answer):
+    if answer == right_answer or answer in right_answer:
+        print('yes')
+        #print(right_sample)
+        return 1
+    else:
+        print('no')
+        print(f'({right_answer})')
+        return 0
+
+
+# show task
+def run(base, lang):
+    #idx, task, right_answer = get_task(base, lang)
+    idx, task, right_answer, sample = get_task(base, lang)
+
+    print_task = ', '.join(task) if type(task) == list else task
+    if SHOW_VARIANTS:
+        print(print_task)
+        variants = base['translate'] if lang == 'ru' else base[['word', 'transcription']]
+        variants.index = range(1, len(base) + 1)
+        print(variants)
+        answer = input('Enter number or word: ')
+        if answer.isdigit():
+            if lang == 'ru':
+                answer = variants.loc[int(answer)]
+            else:
+                answer = variants.loc[int(answer), 'word']
+    else:
+        answer = input(f'{print_task}: ')
+
+    check = get_check(answer, right_answer)
+    print(sample)
+
+    return {'check': check, 'idx': idx, 'task': task,
+            'right_answer': right_answer,
+            'answer': answer, 'lang': lang}
+
+
+# start program
+def go():
+    #lang = rnd.choice(['en', 'ru'])
+    lang = 'en'
+    base, base_add = get_base()
+    log = run(base, lang)
+    to_log(log)
+    base_update(base, base_add, log)
+
+
+SHOW_VARIANTS = False    # показывать ли варианты ответов
+VARS_NUM = 5
+BASE_FILE = os.path.join(os.getcwd(), 'my_dict.csv')
+BASE_FILE_RESERVE = os.path.join(os.getcwd(), 'my_dict_r.csv')
+LOG_FILE = os.path.join(os.getcwd(), 'log.csv')
+
+
+pd.set_option('display.expand_frame_repr', False)
+
+
+
+while 1:
+    go()
+
+
+
+
