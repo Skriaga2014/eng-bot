6b8673b0a918eca4aa34117183ce368c0511e760
skriaga <ilia.salomatin.1987@gmail.com>
2023-01-28 10:23:44 -0500

    alpha_0.2

--- temp.py
+++ temp.py
@@ -1,4 +1,14 @@
-import eng
+import pandas as pd
+import selenium
 
-eng.go()
+
+# with open('newfile.csv', 'r') as newfile:
+#     base = newfile.re
+pd.set_option('display.expand_frame_repr', False)
+base = pd.read_csv('newfile.csv', sep=';', na_filter=False)
+base = base.loc[(base['error'] == '') & (base['status'] == 'word'), ['num', 'eng', 'rus', 'status']]
+#print(base[base['error'] == '', ['num', 'eng', 'rus', 'status']])
+print(base)
+#print(base)
+
 
